3fdbf5acc6dcbffcfd70fffd74b8076bf77bfb95
oizin <fitzgeraldoisin@gmail.com>
2016-12-01 16:02:39 +1100

    first commit, a test

---
+++ R/hello.R
@@ -0,0 +1,18 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn mo=e about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcut for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
