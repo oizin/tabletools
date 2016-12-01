36d031f5d83bc8862d60c4a2dd855e58a32e0795
oizin <fitzgeraldoisin@gmail.com>
2016-12-01 16:42:23 +1100

    add goodbye

--- R/hello.R
+++ R/hello.R
@@ -3,16 +3,18 @@
 # This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
-# You can learn mo=e about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
 #
 # Some useful keyboard shortcut for package authoring:
-#
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
   print("Hello, world!")
+}
+
+goodbye <- function() {
+  print("bye")
 }
